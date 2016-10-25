7e43c75faa5f699d08fd13c8c5bfb9c38269389c
Jesse Shellabarger <shellajt@rose-hulman.edu>
2016-10-24 23:02:42 -0400

    Added navbar for list view

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,30 @@
+var rh = rh || {};
+rh.sl = rh.sl || {};
+
+rh.sl.sideNavShown = false;
+
 $(document).ready(function() {
     console.log("Hello, JavaScript world!");
 });
+
+function toggleNav() {
+	if (!rh.sl.sideNavShown) {
+		openNav();
+		rh.sl.sideNavShown = true;
+	} else {
+		closeNav();
+		rh.sl.sideNavShown = false;
+	}
+}
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
